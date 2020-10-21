631c60b29dceea0c6f78a90398ccbd167d4a4548
전영준 <yjjeon@wemakeprice.com>
2020-10-21 18:52:23 +0900

    계곡 최대 깊이 구하기 - O(n)

---
+++ src/test/java/cote/codility/challenge/FloodDepthTest.java
@@ -0,0 +1,32 @@
+package cote.codility.challenge;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FloodDepthTest {
+
+    @Test
+    public void testMaxFloodDepthEx1() {
+        int[] mountain = {1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2};
+        FloodDepth fd = new FloodDepth();
+        int maxDepth = fd.maxFloodDepth(mountain);
+        assertThat(maxDepth).isEqualTo(2);
+    }
+
+    @Test
+    public void testMaxFloodDepthEx2() {
+        int[] mountain = {5, 8};
+        FloodDepth fd = new FloodDepth();
+        int maxDepth = fd.maxFloodDepth(mountain);
+        assertThat(maxDepth).isZero();
+    }
+
+    @Test
+    public void testMaxFloodDepthEx3() {
+        int[] mountain = {6, 5, 7, 8, 6, 2, 4, 6, 5, 7, 8, 6, 2, 4};
+        FloodDepth fd = new FloodDepth();
+        int maxDepth = fd.maxFloodDepth(mountain);
+        assertThat(maxDepth).isEqualTo(6);
+    }
+}
